d00207682fbcba1536c4c16b3ec00428f80abef2
Muhammad Talha <khokhartalha114@gmail.com>
2024-10-29 15:52:34 +0500

    sign-in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <h1 className="text-3xl font-bold underline">
+//       Hello world!
+//     </h1>
+//   );
+// }
+
+// export default App;
+
+
+import "./App.css";
+import LoginForm from "./pages/Login";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LoginForm />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
